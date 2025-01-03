a72b1fd6d9119e3fcc4cb234890e87a137e72aa6
ritaurbani <ritaurbani@gmail.com>
2025-01-03 15:29:38 +0100

    changes

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# React + Vite
-
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+# Todos
+- create numbers Board to display all numbers
+- extract number 
+- display extracted number
+- filter the original array to exclude the numbers extracted
+- buttons: extract numbers - finish game button
+- bootstrap - css
--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+//index parte da 0 quindi incremento
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  //creo array di numeri da 1-90 ()
+  const tombolaNumbersArray = Array.from({ length: 90 }, (_, index) => index + 1);
+
+  const [isExtracted, setIsExtracted] = useState(false)
+
+  //Estraggo numero
+    const getRandomNumber = (min, max) => {
+      const randomNumber = Math.floor(Math.random() * (max - min) + min)
+      return randomNumber
+    }
+
+    // creo array di numeri rimasti dopo extraction
+   
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='d-flex justify-content-between'>
+        <div>
+          {/* Utilizzo map per creare un elemento per ogni numero */}
+          {tombolaNumbersArray.map((number, index) => (
+            <div 
+              key={index}
+              className='m-2'>
+              {number}
+            </div>
+          ))}
+        </div>
+        <div>
+          <button className='m-4' onClick={{getRandomNumber}}>Extract Number</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
